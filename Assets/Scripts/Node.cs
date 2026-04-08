a4bbd2a470d2ac774ba9481e647036cbe9f6b724
sowTecsup <jneyra@tecsup.edu.pe>
2026-04-08 09:02:15 -0500

    DoubleLinkedList

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -4,7 +4,8 @@ public class Node<T>
 {
     private T value = default;
 
-    private Node<T> next;
+    private Node<T> next = null;
+    private Node<T> prev = null;
 
     public Node (T value)
     {
@@ -14,6 +15,11 @@ public class Node<T>
     {
         this.next = next;
     }
+    public void SetPrev(Node<T> prev)
+    {
+        this.prev = prev;
+    }
     public Node<T> Next => next;
+    public Node<T> Prev => prev;
     public T Value => value;
 }
