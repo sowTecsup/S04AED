8bde89654c6780aae3c19d640cffea3292d1ca5b
sowTecsup <jneyra@tecsup.edu.pe>
2026-04-08 09:26:05 -0500

    Gm

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,79 +5,47 @@ using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
-    //public LinkedList<string> ListaDeNombres = new();
-
-    public Enemy enemyPref;
-
-    public CustomLinkedList HordaManagment;
+    public List<string> Inputs = new List<string>(); 
 
 
-    public List<NodeOfHorde> Nodes;
+    public DoubleLinkedList<string> Nombres;
+    public CircularDoubleLinkedList<string> Dialogos;
+
     void Start()
     {
-        HordaManagment.Set(enemyPref);
-
-        //HordaManagment.ad
-        foreach (var horde in Nodes)
-        {
-            HordaManagment.Add(horde);
-        }
-
-        print(HordaManagment.Count);
+      foreach (var input in Inputs)
+      {
+         Nombres.Add(input);
+         Dialogos.Add(input);
+      }
     }
     void Update()
     {
 
     }
-
     [Button]
-    public void TestMethod(string name, int value)
+    public void ShowDoubleList()
     {
-        Debug.Log("Eureka");
+        Nombres.TraverseInOrder(value => Debug.Log(value.Value) );
+
+        Nombres.TraverseInReverse(value => Debug.Log(value.Value));
+    }
+    [Button]
+    public void RemoveFirst()
+    {
+        Nombres.RemoveFirst();  
+    }
+    [Button]
+    public void RemoveLast()
+    {
+        Nombres.RemoveLast();
+    }
+    [Button]
+    public void AddCustom(string value)
+    {
+        Nombres.Add(Value);
     }
 
 
-    [Button]
-    public void TestNodes()
-    {
-        Node<string> a = new("juan");
-        Node<string> b = new("juan2");
-        Node<string> c = new("juan3");
-        Node<string> d = new("juan4");
-        Node<string> e = new("juan5");
-        a.SetNext(b);
-        b.SetNext(c);
-        c.SetNext(d);
-        d.SetNext(e);
 
-        Node<string> Evaluator = a;
-        while (Evaluator != null)
-        {
-            Debug.Log(Evaluator.Value);
-            Evaluator = Evaluator.Next;
-        }
-    }
-
-    [Button]
-    public void TestLinkedList(List<string> ListaNombres)
-    {
-        LinkedList<string> ListaDeNombres = new();
-        foreach (string n in ListaNombres)
-        {
-            ListaDeNombres.Add(n);
-        }
-        Debug.Log(ListaDeNombres.Count);
-        //ListaDeNombres.RemoveFirst();
-        ListaDeNombres.RemoveLast();
-
-        ListaDeNombres.Traverse(value => Debug.Log(value.Value));
-
-    }
-
-    [Button]
-    public void HordeSpawnTest()
-    {
-        //NodeOfHorde hordaA = new();
-        HordaManagment.SpawnHorde();
-    }
 }
